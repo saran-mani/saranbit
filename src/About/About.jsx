fe8ea14522e4ccc9e476e1755f25a95dc0152b20
saran-mani <saranmvdm@gmail.com>
2023-05-04 12:01:24 +0530

    test1

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -1,1 +1,25 @@
-componen
+import { Component } from "react";
+
+class About extends Component {
+  state = {};
+  render() {
+    return (
+      <>
+        <div className="container text-center">
+          <div className="row">
+            <h1>About</h1>
+            <h2>We Are</h2>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus
+              tenetur aliquam pariatur facilis. Vero totam amet possimus
+              delectus quis deleniti reiciendis similique, officia pariatur non
+              quisquam in saepe quaerat! Temporibus.
+            </p>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+export default About;
